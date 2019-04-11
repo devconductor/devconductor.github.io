b1c70d2aa03c5d65fc74c6ca3de23c5662d6a99f
Dijalma Silva <dijalmacz@gmail.com>
2019-04-11 11:30:11 -0300

    Update date about event

--- utils/config.js
+++ utils/config.js
@@ -162,7 +162,7 @@ const site = {
                 title: 'Programação do Hackathon!',
                 description: 'Fiquem atentos aos horários.',
                 topics: [
-                    'Inscrições - 08/04/2019 à 12/04/2019',
+                    'Inscrições - 12/04/2019 à 19/04/2019',
                     'Abertura/Apresentação - 19:00 à 20:00 do dia 29/04/2019',
                     'Mão na massa - 30/04/2019 à 04/05/2019',
                     'Apresentações e resultados - 05/05/2019',
